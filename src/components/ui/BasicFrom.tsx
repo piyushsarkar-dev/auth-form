cb9b6ad9c116f2a32761a36ae7cbb2feeb0c8045
Mr Piyush <hi.mrpiyush@gmail.com>
2026-03-10 02:03:21 +0530

    Update BasicFrom.tsx

--- src/components/ui/BasicFrom.tsx
+++ src/components/ui/BasicFrom.tsx
@@ -1,21 +1,6 @@
 "use client";
 
-import { MainloginFromSceamType, mainLoginFromSchema } from "@/lib/zodSchema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-
 const BasicFrom = () => {
-  const { handleSubmit } = useForm({
-    resolver: zodResolver(mainLoginFromSchema),
-    defaultValues: {
-      fullName: "",
-      email: "",
-    },
-  });
-  const formHandlar = (fData: MainloginFromSceamType) => {
-    console.log(fData);
-  };
-
   return <section></section>;
 };
 
